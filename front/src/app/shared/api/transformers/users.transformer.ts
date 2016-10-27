dd4bed106f027772ac75347f18fa2d02b36cb0bf
VolosUra <u.volos@rozumsoft.by>
2016-10-27 09:00:15 +0300

    [admin/users] : the transformer was not added in commit

---
+++ front/src/app/shared/api/transformers/users.transformer.ts
@@ -0,0 +1,30 @@
+import { ITransformer } from './interface.transformer';
+import { User } from '../../models/user';
+
+
+export class UsersTransformer implements ITransformer {
+    transform(data: any) : Array<User> {
+        if (data) {
+            let users = Array<User>();
+
+            for(let item of data) {
+                let user = new User(item.id,
+                    item.email,
+                    item.roles,
+                    item.password,
+                    item.first_name || '',
+                    item.last_name || '',
+                    item.middle_name || '',
+                    item.phone || '',
+                    item.city || '',
+                    item.birthday || '');
+
+                users.push(user);
+            }
+
+            return users;
+        }
+
+        return null;
+    }
+}
