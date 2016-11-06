a7ce328c066bca9df1e81fd627f3efa377a8ae00
Kruglik Denis <kruglikdenis1995@gmail.com>
2016-11-06 14:22:10 +0300

    добавил регистрацию

---
+++ front/src/app/shared/validation/messages/validator.messages.ts
@@ -0,0 +1,14 @@
+export const validationMessages = {
+    email: {
+        required: 'Email is required.',
+        incorrectMailFormat: 'Incorrect email format.'
+    },
+    password: {
+        required: 'Password is required.',
+        minlength: 'Length should be greater than 6 characters.'
+    },
+    repeatPassword: {
+        required: 'Repeat password is required.',
+        areEqual: 'Passwords should be equal.'
+    }
+};
