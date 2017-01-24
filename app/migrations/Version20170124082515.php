aa46d0076e140b3817d8910dd151071e64407321
u.volos <u.volos@rozumsoft.by>
2017-01-24 15:53:37 +0300

    [project-tasks] - fix

---
+++ app/migrations/Version20170124082515.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170124082515 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_5058659777153098 ON tasks');
+        $this->addSql('ALTER TABLE tasks CHANGE code code VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tasks CHANGE code code VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_5058659777153098 ON tasks (code)');
+    }
+}
